062c6a2994a7a2ac5a8a92d4d94334b62fbdf60b
cea89585 <cea89585@gmail.com>
2026-05-07 16:54:50 +0200

    v 0.0078

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -56,7 +56,7 @@ export default function ClientLayout({
           </GameProvider>
         </FirebaseProvider>
         <Toaster />
-        <Analytics />
+        {process.env.NODE_ENV === 'production' ? <Analytics /> : null}
       </body>
     </html>
   );
